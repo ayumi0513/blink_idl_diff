a8dbad27b32a4e7b465a5bab5ddaf75d87c04174
Ayumi Shimada <shimadaa@google.com>
2015-08-13 16:15:11 +0900

    get operation, arguments and type

--- Ayumi.py
+++ Ayumi.py
@@ -73,26 +73,49 @@ def find_idlfnames(idlfname_interface):
                 interface_idlfname[interface] = [idlfname] 	
     return interface_idlfname
 
+
+
 def get_interfaces(node):
     return node.GetListOf('Interface')
 
-def get_attributes(interface):
-    return interface.GetListOf('Attribute')
+def get_attributes(interface_node):
+    return interface_node.GetListOf('Attribute')
 
 
-def get_type(attribute):
-    return attribute.GetChildren()[0].GetChildren()[0].GetName() 
+def get_type(node):
+    type_node = node.GetListOf('Type')[0]
+    return type_node.GetChildren()[0] 
 
 
 #in this function, we use get_interfaces(),get_attributes(),get_type()
 def output_interfaces_attributes_types(node_and_idlfname):
-    attributes = []
     for node, idlfname in node_and_idlfname:
         for interface_node in get_interfaces(node):
             print interface_node.GetName()
             for attribute_node in get_attributes(interface_node):
-                print '  a:', attribute_node.GetName(), '    t:', get_type(attribute_node)
+                print '  a:', attribute_node.GetName(), '    t:', get_type(attribute_node).GetName()   
 
+def get_operations(interface_node):
+    return interface_node.GetListOf('Operation')
+
+def get_arguments(operation_node):
+    arguments = []
+    arguments_node = operation_node.GetListOf('Arguments')[0]
+    for argument_node in arguments_node.GetListOf('Argument'):
+        arguments.append(argument_node)
+    return arguments
+
+def output_interfaces_operations(node_and_idlfname):
+    for node, idlfname in node_and_idlfname:
+        for interface_node in get_interfaces(node):
+            print interface_node.GetName()
+            for operation_node in get_operations(interface_node):        
+                print '  type:',get_type(operation_node).GetName(),' ,operation:', operation_node.GetName()
+                for argument_node in get_arguments(operation_node):
+                    print '    argtype:', get_type(argument_node).GetName()  ,' ,arg:', argument_node.GetName()
+                    print '    this idl file is:', idlfname
+                        
+                     
 
 def main(argv):
     dir_name = argv[0]
@@ -106,9 +129,9 @@ def main(argv):
             sys.exit()
     print 'All idl files include ONLY ONE interface name'
     #print find_idlfnames(idlfname_interface_dict)
-    output_interfaces_attributes_types(node_and_idlfname_list)    
-    #node_of_interface = get_node_of_interface(node_and_idlfname_list)
-    #get_node_of_attribute(node_of_interface)
+    #output_interfaces_attributes_types(node_and_idlfname_list)    
+    output_interfaces_operations(node_and_idlfname_list)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])

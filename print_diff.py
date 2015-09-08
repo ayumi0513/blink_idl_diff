10aa18e56a851a4413e47d09c2070b4d1781d574
Ayumi Shimada <shimadaa@google.com>
2015-09-08 17:28:55 +0900

    Added some comments

--- print_diff.py
+++ print_diff.py
@@ -2,18 +2,23 @@ import json
 import sys
 from collections import OrderedDict
 
-def get_json_data(filepath):
+
+def load_json_data(filepath):
     """load a json file into a dictionary
-    Args:
-        filepath:a file path that we want to load
+    Arg : A file path of a json file that we want to load
     Return:
-        a dictionary that was included in a json file
+        a dictionary that is loaded from the json file
     """
     with open(filepath,'r') as f:
         return json.load(f)
 
 
 def sort_member(interface_content):
+    """Sort the order of members like below.
+       [ExtAttributes->Const->Attribute->Operation]
+        Arg : The content of an interface
+        Return : The sorted content of an interface
+    """
     sorted_interface_content = OrderedDict()
     extattributes = []
     const = []
@@ -36,7 +41,12 @@ def sort_member(interface_content):
         sorted_interface_content['diff_tag'] = interface_content['diff_tag']
     return sorted_interface_content
 
+
 def sort_diff_data_by_tags(diff_data):
+    """Sort diff_data using diff_tag.
+        Arg : A dictionary that is loaded by load_json_data().
+        Return : A sorted dictionary
+    """
     sorted_diff = OrderedDict()
     interface_name_deleted_whole = []
     interface_name_added_whole = []
@@ -83,13 +93,23 @@ def sort_diff_data_by_tags(diff_data):
 
 
 def replace_diff_tag(diff_tag):
+    """If diff_tag is 'deleted', replace 'deleted' for '-'. 
+       If diff_tag is 'added', replace 'added' for '+'.
+       Arg : 'deleted' or 'added'
+       Return : '-' or '+'
+    """
     if diff_tag == 'deleted':
         diff_tag = '-'
     else:
         diff_tag = '+'
     return diff_tag
 
+
 def whether_diff_tag_exist(content):
+    """judge whether diff_tag is exist or not.
+        Arg : A dictionary that possibly has diff_tag.
+        Return : If input has 'diff_tag' in that of keys, return that of value. Otherwise return nothing.
+    """
     if 'diff_tag' in content.keys():
         diff_tag = replace_diff_tag(content['diff_tag'])
     return diff_tag
@@ -97,6 +117,9 @@ def whether_diff_tag_exist(content):
 
 
 def print_extattribute(extattributes):
+    """Print a content of extattribute.
+        Arg : A list consists of a content of an extattribute
+    """
     for extattributes_content in extattributes:
         if 'diff_tag' in extattributes_content.keys():
             diff_tag = replace_diff_tag(extattributes_content['diff_tag'])
@@ -107,6 +130,9 @@ def print_extattribute(extattributes):
 
 
 def print_const(const):
+    """Print a content of const.
+        Arg : A list consists of a content of a const
+    """
     for const_content in const:
         if 'diff_tag' in const_content.keys():
             diff_tag = replace_diff_tag(const_content['diff_tag'])
@@ -119,6 +145,9 @@ def print_const(const):
 
 
 def print_extattributes_of_attribute_and_operation(extattributes):
+    """Print extattributes in attribute or operation.
+        Arg : A dictionary whose key is 'ExtAttributes' and value is a list of extattributes
+    """
     sys.stdout.write('[')
     count = 0
     for extattributes_content in extattributes:
@@ -128,7 +157,11 @@ def print_extattributes_of_attribute_and_operation(extattributes):
             print ', ',
     print ']',
 
+
 def print_attribute(attribute):
+    """Print a content of attribute.
+        Arg : A list consists of a content of an attribute
+    """
     for attribute_content in attribute:
         if 'diff_tag' in attribute_content.keys():
             diff_tag = replace_diff_tag(attribute_content['diff_tag'])
@@ -140,7 +173,11 @@ def print_attribute(attribute):
         print attribute_content['Type'],
         print attribute_content['Name']
 
+
 def print_argument(argument):
+    """Print an argument in an operation.
+        Arg : A dictionary whose key is 'Argument' and value is a list of argument
+    """
     count = 0
     sys.stdout.write('(')
     for argument_content in argument:
@@ -151,7 +188,11 @@ def print_argument(argument):
             print ',',
     print (')')
 
+
 def print_operation(operation):
+    """Print a content of an operation
+        Arg : A list consists of a content of an operation
+    """
     for operation_content in operation:
         if 'diff_tag' in operation_content.keys():
             diff_tag = replace_diff_tag(operation_content['diff_tag'])
@@ -169,6 +210,9 @@ def print_operation(operation):
 
 
 def print_diff(diff):
+    """Print the diff on a shell.
+        Arg : A sorted dictionary  
+    """
     for interface_name, interface_content in diff.items():
         if 'diff_tag' in interface_content.keys():
             diff_tag = replace_diff_tag(interface_content['diff_tag'])
@@ -190,15 +234,20 @@ def print_diff(diff):
                 print_operation(member_content)
 
 
-def make_json_file(data, filename):
-    with open(filename, 'w') as f:
+def make_json_file(data, filepath):
+    """Make a json file consiste of dictionary.
+        Args : 
+            data : A sorted dictionary
+            filepath : A file path that we want to make
+    """
+    with open(filepath, 'w') as f:
         json.dump(data, f, indent=4)
         f.close
 
 
 def main(argv):
     json_data = argv[0]
-    diff_data = get_json_data(json_data)
+    diff_data = load_json_data(json_data)
     sorted_diff = sort_diff_data_by_tags(diff_data)
     make_json_file(sorted_diff, 'sorted.json')
     print_diff(sorted_diff)
